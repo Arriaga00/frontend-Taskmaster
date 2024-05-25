584477b5d234ece23fe815107b5616b4fbdd8ac3
andres arriaga <andresfelipearriaga@gmail.com>
2024-05-25 15:36:01 -0500

    trabajabdio en la sesion del todo

--- src/components/dashboard/main/MainToDo.jsx
+++ src/components/dashboard/main/MainToDo.jsx
@@ -1,21 +1,31 @@
-import { SvgIconView } from "../../global/svg";
+import { SvgIconSearh, SvgIconView } from "../../global/svg";
+import CreateTask from "./sectionToDo/CreateTask";
 import ToDo from "./sectionToDo/ToDo";
 
 const MainToDo = () => {
   return (
     <>
       <section className="w-full h-full p-5  text-xl font-bold sectionTodo">
-        <div className=" w-full border-b border-b-[#242424]">
+        <div className=" w-full border-b border-b-[#242424] flex justify-between items-center">
           <h1 className=" h-10 flex justify-start  text-3xl items-center gradient-text  w-64">
             Todas tus tareas
           </h1>
+          <div>
+            <label htmlFor="search" className="flex items-center gap-1">
+              <SvgIconSearh />
+              <input
+                id="search"
+                type="text"
+                placeholder="buscar"
+                className="w-full bg-transparent  focus:outline-0 placeholder-[#7A7A7A] text-sm  text-[#7A7A7A] "
+              />
+            </label>
+          </div>
         </div>
         <main className="w-full h-[97%] flex mainTodo">
-          <section className="w-[50%] h-full border-r border-r-[#242424] py-5 pr-5 relative">
+          <section className="w-[50%] h-full border-r border-r-[#242424] py-5 pr-5 relative overflow-y-auto">
             <ToDo />
-            <div className="bottom-0 left-0 right-0 absolute w-full flex justify-center items-center mb-2 p-3">
-              Contacts
-            </div>
+            <CreateTask />
           </section>
           <section className="w-[50%] h-full flex justify-center items-center font-bold text-[#7A7A7A] text-lg py-5 pl-5">
             Vista de tareas <SvgIconView />
--- src/components/global/svg.jsx
+++ src/components/global/svg.jsx
@@ -92,3 +92,22 @@ export const SvgIconView = () => {
     </svg>
   );
 };
+
+export const SvgIconSearh = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth={1.5}
+      stroke="currentColor"
+      className="size-8  text-[#242424]"
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z"
+      />
+    </svg>
+  );
+};
